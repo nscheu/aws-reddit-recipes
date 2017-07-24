ea2f61cabd5b3d94590cdc8443153fede38e1083
nscheu <scheuring.n@husky.neu.edu>
2017-07-24 08:39:44 -0400

    still trying to get nginx reverse proxy to work with >1 node servers

--- appPlannerApp/server.js
+++ appPlannerApp/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3010;
+const PORT = 3000;
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
--- gifRecApp/index.js
+++ gifRecApp/index.js
@@ -11,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 //var async = require('async');
 //var multer = require('multer');
 
-const PORT = 3000;
+const PORT = 4000;
 var fs = require('fs');
 
 
