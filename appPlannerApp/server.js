279899bbd87e21e35c059756756cb2160c56f242
nscheu <scheuring.n@husky.neu.edu>
2017-07-24 04:21:22 -0400

    simple express test server

--- appPlannerApp/server.js
+++ appPlannerApp/server.js
@@ -1,17 +1,10 @@
-const http = require('http');
-const port = 3010;
+const express = require('express')
+const app = express()
 
-const requestHandler = (request, response) => {
-    console.log(request.url);
-    response.end('Hello Node.js Server!')
-};
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
 
-const server = http.createServer(requestHandler);
-
-server.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err)
-    }
-
-    console.log(`server is listening on ${port}`)
-});
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
